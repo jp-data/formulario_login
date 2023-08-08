e692fb755dbf3b986e7f4535cee7a931674e459b
joao <joaopaulo94.economia@gmail.com>
2023-08-07 22:23:35 -0300

    CSS + DOM - Editar perfil e incluir experiência

--- public/javascripts/editPerfil.js
+++ public/javascripts/editPerfil.js
@@ -1,8 +1,14 @@
 const experience2 = document.getElementById('experience2');
+const btn2 = document.querySelector('#add-experience2')
 const experience3 = document.getElementById('experience3')
-const btn = document.querySelector('#add-experience')
+const btn3 = document.querySelector('#add-experience3')
 
-btn.addEventListener('click', (e) => {
+btn2.addEventListener('click', (e) => {
     e.preventDefault()
     experience2.style.visibility = 'visible'
-})
+});
+
+btn3.addEventListener('click', (e) => {
+    e.preventDefault()
+    experience3.style.visibility = 'visible'
+});
