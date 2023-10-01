a347f999db96b292e381931fc3aabd9076e6facc
joao <joaopaulo94.economia@gmail.com>
2023-10-01 19:31:24 -0300

    troca de botões ao salvar experiência - DOM

--- public/javascripts/editPerfil.js
+++ public/javascripts/editPerfil.js
@@ -4,6 +4,9 @@ const btn = document.getElementById('add-experience');
 const experience1 = document.getElementById('experience1');
 const btnDiscardEx1 = document.querySelector('#discard-experience1')
 
+const btnSaveExperience = document.getElementById("save-experience1")
+
+
 
 btn.addEventListener('click', (e) => {
     e.preventDefault()
@@ -18,3 +21,13 @@ btnDiscardEx1.addEventListener('click', (e) => {
     experience1.style.visibility = 'hidden'
     
 });
+
+btnSaveExperience.addEventListener('click', (e) => {
+    e.preventDefault()
+    btn.disabled = false
+    document.getElementById('save-experience1').innerHTML = `<i class="ph ph-pen"></i>`
+    document.getElementById('save-experience1').title = 'Editar'
+    document.getElementById('discard-experience1').innerHTML = `<i class="ph ph-trash"></i>`
+    document.getElementById('discard-experience1').title = 'Excluir'
+
+})
